311dff894e64d03d735d11e7623dd5157dcd7407
Akash Yadav <itsaky01@gmail.com>
2022-04-20 22:24:51 +0530

    Return actual implementations in AndroidFsProvider

--- src/jdk.compiler/share/classes/com/sun/tools/javac/file/AndroidFsProvider.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/file/AndroidFsProvider.java
@@ -1,17 +1,26 @@
 package com.sun.tools.javac.file;
 
+import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.spi.FileSystemProvider;
 
+import com.itsaky.androidide.zipfs.ZipFileSystemProvider;
+import jdk.internal.jrtfs.JrtFileSystemProvider;
+
 public class AndroidFsProvider {
 
+    // Can be overridden to provide custom implementation
     public static AndroidFsProvider INSTANCE = new AndroidFsProvider();
 
     public FileSystemProvider zipFsProvider () {
-        throw new UnsupportedOperationException("Not implemented!");
+        return new ZipFileSystemProvider();
     }
 
     public FileSystem jrtFileSystem () {
-        throw new UnsupportedOperationException("Not implemented!");
+        try {
+            return new JrtFileSystemProvider().newFileSystem(URI.create("jrt:/"), new HashMap<>(0));
+        } catch (IOException e) {
+            throw new RuntimeException ("Unable to create instance of JRTFileSystem", e);
+        }
     }
 }
