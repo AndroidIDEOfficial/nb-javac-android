69eb4e46d7600b7d6f91804773f4e12a2e71f29d
Akash Yadav <itsaky01@gmail.com>
2022-08-05 18:49:32 +0530

    Use buildscript to build in GitHub actions

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ export ant_dir=./make/langtools/netbeans/nb-javac
 
 # Build nb-javac-android
 JDK_8="/usr/lib/jvm/java-8-openjdk-amd64"
+
+if [[ -z "$CI" ]]; then
+  JDK_8=$JAVA_8
+fi
+
 if [ -d "$JDK_8" ]; then
   export JAVA_HOME=$JDK_8
 fi
